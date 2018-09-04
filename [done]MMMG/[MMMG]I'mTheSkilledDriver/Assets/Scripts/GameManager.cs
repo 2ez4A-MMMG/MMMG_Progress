6cc7990a052437cd1c9ffb9b1999b04601dcb551
HegbackAng <angcheesiah@gmail.com>
2018-09-04 21:34:00 +0800

    done
    I hope...again.

---
+++ [done]MMMG/[MMMG]I'mTheSkilledDriver/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public BoardManager boardScript;
+	public static GameManager managerInstance = null;
+
+	#region SceneSetup
+	// Use this for initialization
+	void Awake () {
+		if (managerInstance == null)
+			managerInstance = this;
+		else if (managerInstance != this)
+			Destroy(gameObject);
+
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	public void InitGame()
+	{
+		boardScript.SetupScene();
+	}
+	#endregion
+	
+	
+}
